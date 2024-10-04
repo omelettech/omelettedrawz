4a44ef309ed1fdd2fa4345b69007b39d5a49e224
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-10-03 23:59:44 -0400

    Updated Contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import './Contact.css';
-
+import PageHeading from "../../components/PageHeading/PageHeading.tsx";
+import image from "../../assets/images/image1.png"
+import BodyCard from "../../components/ProductCard/BodyCard";
+import SectionHeading from "../../components/SectionHeading/SectionHeading.tsx";
 function Contact() {
-  return (
-    <div className="contact-container">
-      <h2 className="contact-title">Contact Us</h2>
-      <form className="contact-form">
-        <input type="text" placeholder="Name" className="contact-input" />
-        <input type="email" placeholder="Email" className="contact-input" />
-        <textarea placeholder="Message" className="contact-textarea"></textarea>
-        <button type="submit" className="contact-button">Send</button>
-      </form>
-    </div>
-  );
+    function handleCardClick() {
+        return undefined;
+    }
+
+    return (
+        <>
+            <PageHeading url={"/static/media/image2.2afdf2e7a647b3e0e510.png"} text={"Commissions"}></PageHeading>
+            <BodyCard onClick={handleCardClick()}
+                      title={"Portrait"}
+                      description={"Drawing of a person"}
+                      img={image}
+                      buttonText={"Contact"}
+                      reverse={true}
+                      hover={false}>
+            </BodyCard>
+            <BodyCard onClick={handleCardClick()}
+                      title={"Custom stickers"}
+                      description={"Have me design you a sticker and ship it"}
+                      img={image} buttonText={"Contact"}
+                      hover={false}>
+
+            </BodyCard>
+            <SectionHeading text={"Other stuff"} align={"center"}></SectionHeading>
+            <div className="contact-container">
+
+                <h2 className="contact-title">Contact Me</h2>
+                <form className="contact-form">
+                    <input type="text" placeholder="Name" className="contact-input"/>
+                    <input type="email" placeholder="Email" className="contact-input"/>
+                    <textarea placeholder="Message" className="contact-textarea"></textarea>
+                    <button type="submit" className="contact-button">Send</button>
+                </form>
+            </div>
+        </>
+
+    );
 }
 
 export default Contact;
