623ab2eb82ada441fc9f9dac75e80190c06727c0
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-10-19 14:36:33 -0400

    Removed firebase from backend

--- api/settings.py
+++ api/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
-import firebase_admin
-from firebase_admin import credentials
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
