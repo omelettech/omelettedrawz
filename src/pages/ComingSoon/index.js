a4a73418c74b564097e4a660a0a6114bde95e139
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-10-19 18:59:42 -0400

    Added coming soon page to shop

--- src/pages/ComingSoon/index.js
+++ src/pages/ComingSoon/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import PageHeading from "../../components/PageHeading/PageHeading";
+import PageHeading from "../../components/PageHeading/PageHeading.tsx";
+import {Link} from "react-router-dom";
 
 const ComingSoon = () => {
     return (
         <>
             <PageHeading url={"/static/media/image2.2afdf2e7a647b3e0e510.png"} text={"Store"}></PageHeading>
-            <div style={{alignContent:"center"}}>
-                Coming soon
+            <div>
+                <h1 align={"center"} style={{margin:"100px", fontSize:"5rem"}}>Coming soon</h1>
+
             </div>
 
         </>
