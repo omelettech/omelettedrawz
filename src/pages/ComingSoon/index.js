e168e12ded390e6c85cce7bbce8c47a908a0f2d3
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-10-20 11:52:02 -0400

    Added .env to gitignore

--- src/pages/ComingSoon/index.js
+++ src/pages/ComingSoon/index.js
@@ -7,7 +7,7 @@ const ComingSoon = () => {
         <>
             <PageHeading url={"/static/media/image2.2afdf2e7a647b3e0e510.png"} text={"Store"}></PageHeading>
             <div>
-                <h1 align={"center"} style={{margin:"100px", fontSize:"5rem"}}>Coming soon</h1>
+                <h1 align={"center"} style={{margin:"100px", fontSize:"5rem",color:'#fe4646'}}>Coming soon</h1>
 
             </div>
 
