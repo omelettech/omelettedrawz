cbe12a42f76089329748798cd226be05f8e22d7c
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-10-24 20:41:58 -0400

    Changed login and register fomrs

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,11 +1,12 @@
 import React, {useContext, useState} from 'react';
 import './Login.css';
-import {useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {auth} from '../../config/firebase';
 import {signInWithEmailAndPassword} from 'firebase/auth';
 import {AuthContext} from '../../context/AuthContext';
 import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 import g_logo from "../../assets/images/google_logo.png"
+
 function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -38,36 +39,47 @@ function SignIn() {
     }
 
     return (
-        <div className="form-container">
-            <h2 className="signin-title">Sign In</h2>
-            {currentUser && <p>Welcome back, {currentUser.email}</p>}
-            {error && <p className="signin-error">{error}</p>}
-            <form className="signin-form" onSubmit={handleSignIn}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    className="signin-input"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="signin-input"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit" className="signin-button">Sign In</button>
-            </form>
-            <br/>
-            <hr/>
-            <p align={'center'}>Or log in with</p>
-            <div style={{display:'flex',flexDirection:"row",justifyContent: 'center'}}>
-                <button onClick={handleGoogleLogin} className={"badge"}><img
-                    src={g_logo}
-                    alt="Google logo"
-                    style={{width: '25px'}}
-                /></button>
+        <div style={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: 0,
+        }}>
+            <div className="form-container">
+                <h2 className="signin-title">Sign In</h2>
+                {currentUser && <p>Welcome back, {currentUser.email}</p>}
+                {error && <p className="signin-error">{error}</p>}
+                <form className="signin-form" onSubmit={handleSignIn}>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="signin-input"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="signin-input"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="submit" className="signin-button">Sign In</button>
+                </form>
+                <br/>
+                <hr/>
+                <p align={'center'}>Or log in with</p>
+                <div style={{display: 'flex', flexDirection: "row", justifyContent: 'center'}}>
+                    <button onClick={handleGoogleLogin} className={"badge"}><img
+                        src={g_logo}
+                        alt="Google logo"
+                        style={{width: '25px'}}
+                    /></button>
+                </div>
+                <p>
+                    Don't an account? <Link to='/register'>Register</Link>
+                </p>
             </div>
         </div>
     );
--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -1,59 +1,94 @@
 import React, {useState} from 'react';
 import './Register.css';
-import { auth } from '../../config/firebase';
-import { createUserWithEmailAndPassword,getAuth } from 'firebase/auth';
-import {Link} from "react-router-dom";
+import {auth} from '../../config/firebase';
+import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import {Link, useNavigate} from "react-router-dom";
+import g_logo from "../../assets/images/google_logo.png";
 
 function Register() {
     const [email, setEmail] = useState('');
-      const [password, setPassword] = useState('');
-      const [username, setUsername] = useState('');
-      const [error, setError] = useState('');
-    const handleRegistration = async (e) => {
-    e.preventDefault();
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const handleGoogleLogin = async (e) => {
+        try {
+            let provider = await new GoogleAuthProvider();
+            signInWithPopup(auth, provider).then(
+                () => navigate('/')
+            ).catch(
+                (e) => setError(e)
+            )
+        } catch (e) {
+            console.error(e)
+        }
 
-    } catch (err) {
-      setError(err.message);
     }
-  };
+    const handleRegistration = async (e) => {
+        e.preventDefault();
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
 
-  return (
-      <div className="register-container">
-          <h2 className="register-title">Register</h2>
-          {error && <p className="register-error">{error}</p>}
-          <form className="register-form" onSubmit={handleRegistration}>
-              <input
-                  type="text"
-                  placeholder="Username"
-                  className="register-input"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-              />
-              <input
-                  type="email"
-                  placeholder="Email"
-                  className="register-input"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                  type="password"
-                  placeholder="Password"
-                  className="register-input"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-              />
-              <button type="submit" className="register-button">Sign Up</button>
-          </form>
-          <div className='signupContainer__box__login'>
-              <p>
-                  Already have an account? <Link to='/signin'>Sign In</Link>
-              </p>
-          </div>
-      </div>
-  );
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
+    return (
+        <div style={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: 0,
+        }}>
+            <div className="form-container">
+                <h2 className="register-title">Register</h2>
+                {error && <p className="register-error">{error}</p>}
+                <form className="register-form" onSubmit={handleRegistration}>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        className="register-input"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="register-input"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="register-input"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="submit" className="register-button">Sign Up</button>
+                </form>
+                <br/>
+                <hr/>
+                <br/>
+                <p align={'center'}>Or Sign up with</p>
+
+                <div style={{display: 'flex', flexDirection: "row", justifyContent: 'center'}}>
+                    <button onClick={handleGoogleLogin} className={"badge"}><img
+                        src={g_logo}
+                        alt="Google logo"
+                        style={{width: '25px'}}
+                    /></button>
+                </div>
+                <div className='signupContainer__box__login'>
+                    <p>
+                        Already have an account? <Link to='/login'>Sign In</Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Register;
