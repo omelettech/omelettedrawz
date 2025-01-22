ea88106e84e4769c62671df3496560e420530be2
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-01-21 20:23:02 -0500

    Style changes

--- src/context/AxiosInstance.js
+++ src/context/AxiosInstance.js
@@ -9,11 +9,8 @@ const axiosInstance = axios.create({
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
     (config) => {
-        console.log("Axios intercepted")
         const token = localStorage.getItem("token"); // Retrieve the token from local storage
         if (token) {
-            console.log("sending header with ",token)
-
             config.headers.Authorization = `Bearer ${token}`;
         }
         return config;
