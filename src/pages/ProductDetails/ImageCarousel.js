4b5ebdd2cf4fd9beb764977461c35cc665d906d1
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-01-23 14:42:32 -0500

    Added new carousel component

---
+++ src/pages/ProductDetails/ImageCarousel.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+const ImageCarousel = ({ images }) => {
+    console.count('counter')
+    let MEDIA_URL = "http://127.0.0.1:8000"
+
+    const scrollBackToFirst=()=>{
+        const swiper = document.querySelector('.swiper').swiper;
+
+        // Now you can use all slider methods like
+        // swiper.slideTo(0,1);
+        console.log(swiper)
+
+    }
+    return (
+        <div className="image-carousel-container">
+            <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => scrollBackToFirst()}
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <img src={MEDIA_URL+image.image} alt={`Product ${index + 1}`} className="product-carousel-image" />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+};
+
+export default ImageCarousel;
