1ec8c311815c059c40ef6125b6630ac6e5744971
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-02-05 00:58:28 -0500

    Added commission forms

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Contact.css';
 import PageHeading from "../../components/PageHeading/PageHeading.tsx";
 import image from "../../assets/images/image1.png"
 import BodyCard from "../../components/ProductCard/BodyCard";
 import SectionHeading from "../../components/SectionHeading/SectionHeading.tsx";
+import ProductDetail from "../ProductDetails/ProductDetails";
+import SmartForm from "../../components/SmartForm/SmartForm";
+import {Form} from "react-router-dom";
+import {StickerForm} from "./StickerForm";
+import {PortraitForm} from "./PortraitForm";
+import {useAuth} from "../../context/AuthContext";
+
+
+
+
 function Contact() {
-    function handleCardClick() {
-        return undefined;
+
+    // const [isModal, setIsModal] = useState(false)
+    const [selectedForm, setSelectedForm] = useState(null); // State to track the active form
+    const {currentUser}=useAuth()
+
+    function handleCardClick(formComponentName) {
+        setSelectedForm(formComponentName)
     }
+
+    const onClose = () => {
+        setSelectedForm(null)
+    }
+    const formComponents = {
+        PortraitForm: <PortraitForm currentUser={currentUser}/>,
+        StickerForm: <StickerForm />,
+        // formC: <FormC />,
+    };
 
     return (
         <>
+            {selectedForm &&
+                <div className="modal-overlay" onClick={onClose}>
+                    <div className="modal-container" onClick={(e) => e.stopPropagation()}>
+                        <span className="modal-close" onClick={onClose}>&times;</span>
+
+                        <SectionHeading text={"Commission request form"} align={"center"}></SectionHeading>
+
+                            {formComponents[selectedForm] || <p>Error rendering form, Contact me instead</p>}
+
+                        {/*<ProductDetail product={selectedProduct} productSku={selectedProduct.default_sku} onClose={onClose}/>*/}
+                    </div>
+                </div>
+            }
             <PageHeading url={"/static/media/image2.2afdf2e7a647b3e0e510.png"} text={"Commissions"}></PageHeading>
-            <BodyCard onClick={handleCardClick()}
+            <BodyCard onClick={()=>handleCardClick("PortraitForm")}
                       title={"Portrait"}
                       description={"Drawing of a person"}
                       img={image}
@@ -20,7 +57,7 @@ function Contact() {
                       reverse={true}
                       hover={false}>
             </BodyCard>
-            <BodyCard onClick={handleCardClick()}
+            <BodyCard onClick={()=>handleCardClick("StickerForm")}
                       title={"Custom stickers"}
                       description={"Have me design you a sticker and ship it"}
                       img={image} buttonText={"Contact"}
