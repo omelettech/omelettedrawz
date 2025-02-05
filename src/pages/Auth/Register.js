f1b5259be6df092ba14abffe5d22014707eda488
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-02-05 00:57:55 -0500

    Fixed form

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -4,13 +4,16 @@ import {auth} from '../../config/firebase';
 import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 import {Link, useNavigate} from "react-router-dom";
 import g_logo from "../../assets/images/google_logo.png";
+import {useAuth} from "../../context/AuthContext";
 
 function Register() {
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('')
     const [username, setUsername] = useState('');
     const [error, setError] = useState('');
     const navigate = useNavigate();
+    const {register}=useAuth()
     const handleGoogleLogin = async (e) => {
         try {
             let provider = await new GoogleAuthProvider();
@@ -24,15 +27,43 @@ function Register() {
         }
 
     }
+    const validateForm = () => {
+        if(email==="" || password1==="" || password2==="" || username===""){
+            setError("All fields are required")
+            return true
+        }
+        if(password1 !== password2){
+            setError("Passwords do not match")
+            return true
+        }
+        if(password1.length < 8){
+            setError("Password must be at least 8 characters")
+            return true
+        }
+        return false
+
+    };
     const handleRegistration = async (e) => {
         e.preventDefault();
-        try {
-            await createUserWithEmailAndPassword(auth, email, password);
+        const validationError = validateForm();
+        if(validationError){
+            return
+        }try {
+            await register(username,email, password1,password2);
+            // navigate("/login")
 
         } catch (err) {
-            setError(err.message);
+            console.log("asdasd")
+            if(err.responseText) {
+                setError(err.responseText);
+            }else {
+                // Other errors (e.g., network issues, bad setup)
+                console.error("Error:", err.message);
+                setError("An unexpected error occurred.");
+            }
         }
     };
+
 
     return (
         <div style={{
@@ -44,29 +75,39 @@ function Register() {
         }}>
             <div className="form-container">
                 <h2 className="register-title">Register</h2>
-                {error && <p className="register-error">{error}</p>}
                 <form className="register-form" onSubmit={handleRegistration}>
+                    <label htmlFor="username">Username</label>
                     <input
                         type="text"
                         placeholder="Username"
-                        className="register-input"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
+                    <label htmlFor="username">Email</label>
+
                     <input
                         type="email"
                         placeholder="Email"
-                        className="register-input"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
+                    <label htmlFor="username">Password</label>
+
                     <input
                         type="password"
                         placeholder="Password"
-                        className="register-input"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        value={password1}
+                        onChange={(e) => setPassword1(e.target.value)}
+
                     />
+                    <input
+                        type="password"
+                        placeholder="Re-enter Password"
+                        value={password2}
+                        onChange={(e) => setPassword2(e.target.value)}
+                    />
+                    {error && <p className="register-error">{error}</p>}
+
                     <button type="submit" className="register-button">Sign Up</button>
                 </form>
                 <br/>
