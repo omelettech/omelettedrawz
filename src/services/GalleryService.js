9e86f735d09cbaca3ab2f45079877abdf82a4333
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-02-27 02:28:23 -0500

    Fixed api url

--- src/services/GalleryService.js
+++ src/services/GalleryService.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from "../context/AxiosInstance";
 import {BASE_URL} from "./apiClient";
 
-const API_URL = BASE_URL+"/images"
+const API_URL = BASE_URL+"images/"
 export const fetchGalleryItems=async ()=>{
     try{
-        const response = await axios.get(API_URL+"/v1/GalleryItem")
+        const response = await axios.get(API_URL+"v1/GalleryItem")
         return response.data
     }catch (e){
         throw e
