e0922abd05003cc093ec082b57a39cbe04b84bbb
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-09-10 21:47:38 -0400

    added db

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -19,5 +21,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
 
+const db = getFirestore(app);
 
-export { app, auth };
+export { app, auth,db };
