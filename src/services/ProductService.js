40023a7b0dffcff6137d9a436205e0fc912aa141
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-09-10 21:53:22 -0400

    moified fetchProduct function to fetch from firebase

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,11 +1,34 @@
 import axios from "axios";
+import {collection, getDocs} from "firebase/firestore";
+import {db} from "../config/firebase";
 
 export const API_URL = "http://127.0.0.1:8000/products/v1/"
 export const fetchProducts = async () => {
     try {
-        const response = await axios.get(API_URL + "products/with_default");
+        // const response = await axios.get(API_URL + "products/with_default");
         // console.log("Products",response.data)
-        return response.data
+        // return response.data
+        const snapshot = await getDocs(collection(db, "products"));
+        return snapshot.docs.map(doc => {
+            // Get the document ID (Firestore assigns this automatically)
+            const documentId = doc.id;
+
+            // Get the data fields (the actual product info you stored in Firestore)
+            const documentData = doc.data();
+            console.log({
+                id: documentId,
+                name: documentData.name,
+                price: documentData.price,
+                imageUrl: documentData.imageUrl
+            })
+            // Build a new object that combines ID + fields
+            return {
+                id: documentId,
+                name: documentData.name,
+                price: documentData.price,
+                imageUrl: documentData.imageUrl
+            };
+        });
     } catch (error) {
         throw error;
     }
