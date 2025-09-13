d063fabb76539dab072bf8268a34e12870fc4439
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-09-13 19:35:39 -0400

    Changed product property name

--- src/pages/Shop/ProductCard.js
+++ src/pages/Shop/ProductCard.js
@@ -3,15 +3,18 @@ import {BASE_URL} from "../../services/apiClient";
 
 const ProductCard = ({product, onClick, getImageSourceCallback}) => {
     const image ={
-        src: BASE_URL+ product.default_sku.associated_image?.image || "default",
-        alt: product.default_sku.associated_image?.alt || "No image"
+        // src: BASE_URL+ product.default_sku.associated_image?.image || "default",
+        // alt: product.default_sku.associated_image?.alt || "No image"
+
+        src:product.src,
+        alt:product.alt
     }
     return (
         <div key={product.id} className="product-card" onClick={onClick}>
             <img src={ image.src } alt={image.alt} className={"product-card-image"}/>
 
             <h3>{product.name}</h3>
-            <p className={"price-tag"}>${product.default_sku.price.toFixed(2)}</p>
+            <p className={"price-tag"}>${product.price.toFixed(2)}</p>
         </div>
     );
 };
