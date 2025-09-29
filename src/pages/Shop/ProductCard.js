d4789d5988c9ecf0152cdbec86a63093b5838df2
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-09-29 13:52:15 -0400

    Changed product structure to match firebase

--- src/pages/Shop/ProductCard.js
+++ src/pages/Shop/ProductCard.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {BASE_URL} from "../../services/apiClient";
+import cart from "../Cart/Cart";
 
-const ProductCard = ({product, onClick, getImageSourceCallback}) => {
+
+const ProductCard = ({product,handleAddToCart}) => {
     const image ={
         // src: BASE_URL+ product.default_sku.associated_image?.image || "default",
         // alt: product.default_sku.associated_image?.alt || "No image"
@@ -10,11 +12,20 @@ const ProductCard = ({product, onClick, getImageSourceCallback}) => {
         alt:product.alt
     }
     return (
-        <div key={product.id} className="product-card" onClick={onClick}>
+        <div key={product.id} className="product-card">
             <img src={ image.src } alt={image.alt} className={"product-card-image"}/>
 
             <h3>{product.name}</h3>
             <p className={"price-tag"}>${product.price.toFixed(2)}</p>
+            <button
+                className="add-to-cart-btn"
+                onClick={(e) => {
+                    e.stopPropagation(); // Prevent triggering the card's onClick
+                    handleAddToCart(product);
+                }}
+            >
+                Add to Cart
+            </button>
         </div>
     );
 };
