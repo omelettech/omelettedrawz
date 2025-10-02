7beecf65f23dc2d8cde9ea9d0b3d449969bdb768
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-10-02 17:24:08 -0400

    Added firebase functions

---
+++ functions/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    "ecmaVersion": 2018,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": "off",
+    "camelcase": "off",
+    "comma-spacing": "off",
+    "indent": "off",
+    "max-len": "off",
+    "require-jsdoc": "off",
+    "valid-jsdoc": "off",
+    "object-curly-spacing": "off",
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
